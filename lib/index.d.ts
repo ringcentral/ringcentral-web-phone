ff75635597a3452ed1966501df094cdeb29f73c3
Jimmy Xu <jimmy.xu@ringcentral.com>
2019-07-08 15:59:41 +0800

    use js

---
+++ lib/index.d.ts
@@ -0,0 +1,57 @@
+import { WebPhoneUserAgent } from './userAgent';
+import { SessionDescriptionHandlerModifiers } from 'sip.js';
+import { WebPhoneSession } from './session';
+import { AudioHelperOptions } from './audioHelper';
+export interface WebPhoneRegData {
+    sipInfo?: any;
+    sipFlags?: any;
+    sipErrorCodes?: string[];
+}
+export interface WebPhoneOptions {
+    uuid?: string;
+    uuidKey?: string;
+    appKey?: string;
+    appName?: string;
+    appVersion?: string;
+    enableUnifiedSDP?: boolean;
+    enableMidLinesInSDP?: boolean;
+    enableQos?: boolean;
+    onSession?: (session: WebPhoneSession) => any;
+    audioHelper?: AudioHelperOptions;
+    modifiers?: SessionDescriptionHandlerModifiers;
+    media?: any;
+    mediaConstraints?: any;
+    sessionDescriptionHandlerFactoryOptions?: any;
+    sessionDescriptionHandlerFactory?: any;
+    maxReconnectionAttempts?: number;
+    reconnectionTimeout?: number;
+    connectionTimeout?: number;
+    keepAliveDebounce?: number;
+    logLevel?: any;
+    builtinEnabled?: boolean;
+    connector?: any;
+    sipErrorCodes?: string[];
+    switchBackInterval?: number;
+    maxReconnectionAttemptsNoBackup?: number;
+    maxReconnectionAttemptsWithBackup?: number;
+    reconnectionTimeoutNoBackup?: number;
+    reconnectionTimeoutWithBackup?: number;
+}
+export default class WebPhone {
+    static version: string;
+    static uuid: () => string;
+    static delay: (ms: number) => Promise<any>;
+    static extend: (dst?: any, src?: any) => any;
+    sipInfo: any;
+    sipFlags: any;
+    uuidKey: string;
+    appKey: string;
+    appName: string;
+    appVersion: string;
+    userAgent: WebPhoneUserAgent;
+    /**
+     * TODO: include 'WebPhone' for all apps other than Chrome and Glip
+     * TODO: parse wsservers from new api spec
+     */
+    constructor(regData?: WebPhoneRegData, options?: WebPhoneOptions);
+}
