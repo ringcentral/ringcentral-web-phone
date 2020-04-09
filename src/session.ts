2126322a5c440db235406d8bf72b8d48c994fdae
Pavel Berezhnoy <43993264+pavelberezhnoy@users.noreply.github.com>
2020-04-09 21:06:51 +0300

    [GLIP-26431] extract name from RC headers (#282)
    Co-authored-by: Pavel Berezhnoy <pavel.berezhnoy@ringcentral.com>

--- src/session.ts
+++ src/session.ts
@@ -23,6 +23,7 @@ export interface RCHeaders {
     to?: string;
     srvLvl?: string;
     srvLvlExt?: string;
+    nm?: string;
     toNm?: string;
     callAttributes?: string;
     srcIVRSiteName?: string;
@@ -273,6 +274,7 @@ const parseRcHeader = (session: WebPhoneSession): any => {
             extend(session.rcHeaders, {
                 srvLvl: bdyNode.getAttribute('SrvLvl'),
                 srvLvlExt: bdyNode.getAttribute('SrvLvlExt'),
+                nm: bdyNode.getAttribute('Nm'),
                 toNm: bdyNode.getAttribute('ToNm')
             });
         }
