4f8532fcf0afd175533272656031903acd62e02d
Tyler Long <tyler4long@gmail.com>
2023-03-09 12:24:49 -0800

    Simplify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
   extends: ['alloy', 'alloy/typescript', 'prettier'],
-  plugins: ['prettier', 'jest'],
+  plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error'],
     // todo: https://github.com/AlloyTeam/eslint-config-alloy/issues/239
     '@typescript-eslint/no-unused-vars': ['error'],
+    // todo: https://github.com/AlloyTeam/eslint-config-alloy/issues/241
     'no-undef': ['off'],
-  },
-  env: {
-    jest: true,
   },
 };
