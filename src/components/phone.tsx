fcc0e124607946760eb05ef2dcb38344a51883c8
Tyler Liu <tyler4long@gmail.com>
2024-07-18 16:07:02 -0700

    useEffect to set callId default value

--- src/components/phone.tsx
+++ src/components/phone.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { auto } from 'manate/react';
 import { Button, Divider, Input, Select, Space, Typography } from 'antd';
+import type { Managed } from 'manate/models';
+import { autoRun } from 'manate';
 
 import type { Store } from '../models/store';
 import CallSession from './call-session';
 
-const Phone = (props: { store: Store }) => {
+const Phone = (props: { store: Managed<Store> }) => {
   const { store } = props;
   const [callee, setCallee] = React.useState<string>('');
-  const [callerId, setCallerId] = React.useState<string>(store.callerIds[0]);
+  const [callerId, setCallerId] = React.useState<string>('');
+  useEffect(() => {
+    const { start, stop } = autoRun(store, () => {
+      if (callerId === '' && store.callerIds.length > 0) {
+        setCallerId(store.callerIds[0]);
+      }
+    });
+    start();
+    return () => stop();
+  }, []);
   const render = () => (
     <>
       <Button id="logout-btn" onClick={() => store.logout()}>
--- src/store.ts
+++ src/store.ts
@@ -3,5 +3,4 @@ import { manage } from 'manate';
 import Store from './models/store';
 
 const store = manage(new Store());
-global.store = store;
 export default store;
