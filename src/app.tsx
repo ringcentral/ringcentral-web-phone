fcce99cac2fafb2b0f19604ef70c1d38e199a589
Tyler Liu <tyler4long@gmail.com>
2024-07-30 17:07:09 -0700

    Display call park result

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Typography } from 'antd';
+import { notification, Typography } from 'antd';
 import { auto } from 'manate/react';
+import type { Managed } from 'manate/models';
 
 import type { Store } from './models/store';
 import Login from './components/login';
 import Phone from './components/phone';
 
-const App = (props: { store: Store }) => {
+const App = (props: { store: Managed<Store> }) => {
   const { store } = props;
+  const [api, contextHolder] = notification.useNotification();
+  global.notifier = api;
   const render = () => (
     <>
+      {contextHolder}
       <Typography.Title>RingCentral Web Phone 2 Demo</Typography.Title>
       {store.rcToken === '' ? <Login store={store} /> : <Phone store={store} />}
     </>
