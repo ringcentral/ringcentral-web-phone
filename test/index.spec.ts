b5b246fd414f889cee1c49802b23d241777b3eac
Tyler Liu <tyler4long@gmail.com>
2024-08-22 14:27:00 -0700

    Test a call

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,31 +3,40 @@ import { expect, test } from '@playwright/test';
 
 import { InboundMessage, type SipMessage } from '../src/sip-message';
 import type WebPhone from '../src';
+import RcMessage from '../src/rc-message/rc-message';
+import callControlCommands from '../src/rc-message/call-control-commands';
+
+const callerJwt = process.env.RINGCENTRAL_JWT_TOKEN!;
+const calleeJwt = process.env.RINGCENTRAL_JWT_TOKEN_2!;
+// const callerNumber = process.env.PHONE_NUMBER!;
+const calleeNumber = process.env.PHONE_NUMBER_2!;
 
 declare global {
   interface Window {
     webPhone: WebPhone;
+    initWebPhone: (jwt: string) => Promise<void>;
   }
 }
 
-const register = async ({ context }: { context: BrowserContext }) => {
+const register = async ({ context, jwt }: { context: BrowserContext; jwt: string }) => {
   const page = await context.newPage();
+  await page.goto('/');
+  await page.evaluate(async (jwt) => {
+    await window.initWebPhone(jwt);
+  }, jwt);
   const messages: SipMessage[] = [];
   page.once('websocket', (ws) => {
     ws.on('framesent', (frame) => messages.push(InboundMessage.fromString(frame.payload as string)));
     ws.on('framereceived', (frame) => messages.push(InboundMessage.fromString(frame.payload as string)));
   });
-  await page.goto('/');
-  await page.waitForTimeout(1000);
   await page.evaluate(async () => {
     await window.webPhone.register();
   });
-  await page.waitForTimeout(1000);
   return { page, messages };
 };
 
 test('registration', async ({ context }) => {
-  const { messages } = await register({ context });
+  const { messages } = await register({ context, jwt: callerJwt });
   expect(messages.length).toBe(6);
   expect(messages.map((m) => m.subject)).toEqual([
     'REGISTER sip:sip.ringcentral.com SIP/2.0',
@@ -40,22 +49,37 @@ test('registration', async ({ context }) => {
 });
 
 test('call', async ({ context }) => {
-  const { page: callerPage, messages: callerMessages } = await register({ context });
+  const { page: callerPage, messages: callerMessages } = await register({ context, jwt: callerJwt });
+  const { messages: calleeMessages } = await register({ context, jwt: calleeJwt });
   callerMessages.length = 0;
-  const callee = process.env.PHONE_NUMBER_2!;
-  await callerPage.evaluate(async (callee) => {
-    await window.webPhone.call(callee);
-  }, callee);
+  calleeMessages.length = 0;
+  await callerPage.evaluate(async (calleeNumber) => {
+    await window.webPhone.call(calleeNumber);
+  }, calleeNumber);
   await callerPage.waitForTimeout(1000);
-  expect(callerMessages.length).toBeGreaterThan(6);
+
+  // caller
+  expect(callerMessages.length).toBe(8);
   expect(callerMessages.map((m) => m.subject)).toEqual([
-    `INVITE sip:${callee}@sip.ringcentral.com SIP/2.0`,
+    `INVITE sip:${calleeNumber}@sip.ringcentral.com SIP/2.0`,
     'SIP/2.0 100 Trying',
     'SIP/2.0 407 Proxy Authentication Required',
-    `INVITE sip:${callee}@sip.ringcentral.com SIP/2.0`,
+    `INVITE sip:${calleeNumber}@sip.ringcentral.com SIP/2.0`,
     'SIP/2.0 100 Trying',
     'SIP/2.0 183 Session Progress',
     'SIP/2.0 200 OK',
-    `ACK sip:${callee}@sip.ringcentral.com SIP/2.0`,
+    `ACK sip:${calleeNumber}@sip.ringcentral.com SIP/2.0`,
   ]);
+
+  // callee
+  expect(calleeMessages.length).toBe(6);
+  expect(calleeMessages[0].subject.startsWith('INVITE sip:')).toBeTruthy();
+  expect(calleeMessages[1].subject).toBe('SIP/2.0 100 Trying');
+  expect(calleeMessages[2].subject).toBe('SIP/2.0 180 Ringing');
+  expect(calleeMessages[3].subject.startsWith('MESSAGE sip:')).toBeTruthy();
+  expect(calleeMessages[4].subject).toBe('SIP/2.0 100 Trying');
+  expect(calleeMessages[5].subject).toBe('SIP/2.0 200 OK');
+
+  const rcMessage = RcMessage.fromXml(calleeMessages[3].body);
+  expect(rcMessage.Hdr.Cmd).toBe(callControlCommands.ClientReceiveConfirm.toString());
 });
--- test/index.ts
+++ test/index.ts
@@ -2,15 +2,13 @@ import RingCentral from '@rc-ex/core';
 
 import WebPhone from '../src';
 
-const main = async () => {
+global.initWebPhone = async (jwt: string) => {
   const rc = new RingCentral({
     server: process.env.RINGCENTRAL_SERVER_URL,
     clientId: process.env.RINGCENTRAL_CLIENT_ID,
     clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
   });
-  await rc.authorize({
-    jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
-  });
+  await rc.authorize({ jwt });
   const r = await rc
     .restapi()
     .clientInfo()
@@ -23,4 +21,3 @@ const main = async () => {
   const webPhone = new WebPhone({ sipInfo });
   global.webPhone = webPhone;
 };
-main();
