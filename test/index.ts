03c534da49b48e70a7414a2bc5188578e2640f5e
Tyler Liu <tyler4long@gmail.com>
2024-09-11 15:15:43 -0700

    Release 2.0.0-alpha.17

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,9 @@
 import { autoRun, manage } from 'manate';
 
 import WebPhone from '../src';
-import { uuid } from '../src/utils';
 
 global.setup = async (sipInfo: string) => {
-  const webPhone = new WebPhone({ sipInfo: JSON.parse(sipInfo), instanceId: uuid(), debug: true });
+  const webPhone = new WebPhone({ sipInfo: JSON.parse(sipInfo), instanceId: 'rc-web-phone-test-uniq-id', debug: true });
   global.webPhone = manage(webPhone);
   const { start, stop } = autoRun(global.webPhone, () => {
     global.inboundCalls = webPhone.callSessions.filter((call) => call.direction === 'inbound');
