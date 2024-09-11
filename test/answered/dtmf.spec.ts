65f441359447744728b0c049457b44af6730af58
Tyler Liu <tyler4long@gmail.com>
2024-09-11 12:56:05 -0700

    Add dtmf test case

---
+++ test/answered/dtmf.spec.ts
@@ -0,0 +1,18 @@
+import { expect } from '@playwright/test';
+
+import { assertCallCount, callAndAnswer, testTwoPages } from '../common';
+
+testTwoPages('dtmf', async ({ callerResource, calleeResource }) => {
+  const { callerPage, calleePage, callerMessages, calleeMessages } = await callAndAnswer(
+    callerResource,
+    calleeResource,
+  );
+
+  await calleePage.evaluate(async () => {
+    await window.inboundCalls[0].sendDtmf('123#');
+  });
+  expect(callerMessages).toHaveLength(0);
+  expect(calleeMessages).toHaveLength(0);
+  await assertCallCount(callerPage, 1);
+  await assertCallCount(calleePage, 1);
+});
