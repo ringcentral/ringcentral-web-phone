575e238702bdab277574341dd848c70f9bc12913
Tyler Liu <tyler4long@gmail.com>
2024-12-05 13:55:18 -0800

    Upgrade dependencies

--- test/index.ts
+++ test/index.ts
@@ -5,7 +5,7 @@ import WebPhone from '../src';
 global.setup = async (sipInfo: string) => {
   const webPhone = new WebPhone({ sipInfo: JSON.parse(sipInfo), instanceId: 'rc-web-phone-test-uniq-id', debug: true });
   global.webPhone = manage(webPhone);
-  const { start, stop } = autoRun(global.webPhone, () => {
+  const { start, stop } = autoRun(() => {
     global.inboundCalls = webPhone.callSessions.filter((call) => call.direction === 'inbound');
     global.outboundCalls = webPhone.callSessions.filter((call) => call.direction === 'outbound');
   });
