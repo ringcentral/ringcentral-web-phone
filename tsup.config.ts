b9567d9f80760d4d4e3e8837bf07867d87906a7a
Tyler Liu <tyler4long@gmail.com>
2026-04-10 17:26:08 -0700

    Build for both ESM and CJS

---
+++ tsup.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "tsup";
+
+const entries = ["src/**/*.ts"];
+
+export default defineConfig([
+  {
+    bundle: false,
+    clean: true,
+    dts: false,
+    entry: entries,
+    format: ["esm"],
+    outDir: "dist/esm",
+    sourcemap: false,
+    target: "es2022",
+    tsconfig: "tsconfig.esm.json",
+  },
+  {
+    bundle: false,
+    clean: false,
+    dts: false,
+    entry: entries,
+    format: ["cjs"],
+    outDir: "dist/cjs",
+    outExtension() {
+      return { js: ".cjs" };
+    },
+    sourcemap: false,
+    target: "es2022",
+    tsconfig: "tsconfig.esm.json",
+  },
+]);
